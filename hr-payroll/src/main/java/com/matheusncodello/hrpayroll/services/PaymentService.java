9d9a9b71d935ccfe47d51874c2a9680ff42f574e
matheusncodello <matheus.ncodello@gmail.com>
2021-08-31 10:13:16 -0300

    UserFeignClient

--- hr-payroll/src/main/java/com/matheusncodello/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/matheusncodello/hrpayroll/services/PaymentService.java
@@ -4,12 +4,8 @@ import com.matheusncodello.hrpayroll.entities.Payment;
 import com.matheusncodello.hrpayroll.entities.Worker;
 import com.matheusncodello.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
